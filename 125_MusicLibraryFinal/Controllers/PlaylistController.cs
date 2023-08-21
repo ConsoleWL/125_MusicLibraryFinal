bc211592b075df6a62615abe3b331036ae43c695
Nikita Obergan <Obergannikita@gmail.com>
2023-08-21 15:51:05 -0400

    Add PlaylistController HttpGet

--- 125_MusicLibraryFinal/Controllers/PlaylistController.cs
+++ 125_MusicLibraryFinal/Controllers/PlaylistController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _125_MusicLibraryFinal.Data;
+using _125_MusicLibraryFinal.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace _125_MusicLibraryFinal.Controllers
 {
@@ -6,6 +9,21 @@ namespace _125_MusicLibraryFinal.Controllers
     [ApiController]
     public class PlaylistController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
 
+        public PlaylistController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Playlist> playlists = _context.Playlists
+                .Include(f => f.Songs)
+                .ToList();
+
+            return Ok(playlists);
+        }
     }
 }
