a3252914aebe906f604cd2df86b9571d29beb043
Nikita Obergan <Obergannikita@gmail.com>
2023-08-21 15:34:37 -0400

    add models  and nav props

---
+++ 125_MusicLibraryFinal/Models/Song.cs
@@ -0,0 +1,19 @@
+﻿namespace _125_MusicLibraryFinal.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public DateTime? DateTime { get; set; }
+        public string? Genre { get; set; }
+        public int Likes { get; set; }
+
+        // navigational props
+
+        public int? PlaylistId { get; set; }
+        public Playlist? Playlist { get; set; }
+
+    }
+}
