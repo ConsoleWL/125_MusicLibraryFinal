117435c83a24898b090b6b362e0cdb37fed1c4eb
Nikita Obergan <Obergannikita@gmail.com>
2023-08-21 15:37:02 -0400

    add on model creating  to aplicationdb context

--- 125_MusicLibraryFinal/Data/ApplicationDbContext.cs
+++ 125_MusicLibraryFinal/Data/ApplicationDbContext.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using _125_MusicLibraryFinal.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace _125_MusicLibraryFinal.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Song>()
+                .HasOne(f => f.Playlist)
+                .WithMany(f => f.Songs)
+                .HasForeignKey(f => f.PlaylistId);
         }
     }
 }
